09eb1779eb57760c535bd607413dc31cd1f85d88
Derek Borski <derek.borski@gmail.com>
2020-07-10 14:41:09 -0600

    Create gem

--- lib/derek_test_gem.rb
+++ lib/derek_test_gem.rb
@@ -2,5 +2,11 @@ require "derek_test_gem/version"
 
 module DerekTestGem
   class Error < StandardError; end
+
+  class Vote
+    def for_me
+      puts "Vote for me!"
+    end 
+  end 
   # Your code goes here...
 end
